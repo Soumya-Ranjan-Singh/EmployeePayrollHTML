bf2bddad70bfa332ce20c63c15d31afe615ffa3c
Soumya Ranjan Singh <soumyars675@gmail.com>
2023-01-01 10:54:28 +0530

    Added : Use Case 6 Program - Ability to view Employee Payroll details from Local Storage

--- JavaScripts/NewHome.js
+++ JavaScripts/NewHome.js
@@ -1,13 +1,22 @@
+let employeePayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    employeePayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ?
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 
 // Template literal ES6 feature
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                         "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    const innerHtml = `${headerHtml}`;
-    let employeePayrollList = createEmployeePayrollJSON();
+    if (employeePayrollList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
     for (const employeePayrollData of employeePayrollList) {
         innerHtml = `${innerHtml}
             <tr>
@@ -35,35 +44,4 @@ const getDeptHtml = (deptList) => {
         deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
     }
     return deptHtml;
-}
-
-const createEmployeePayrollJSON = () => {
-    let employeePayrollListLocal = [
-        {
-            _name: 'Soumya Ranjan',
-            _gender: 'Male',
-            _department: [
-                'Engineering',
-                'HR'
-            ],
-            _salary: '300000',
-            _startDate: '1 July 2020',
-            _note: '',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/profile-images/Ellipse -2.png'
-        },
-        {
-            _name: 'Sambit Behera',
-            _gender: 'Female',
-            _department: [
-                'Sales'
-            ],
-            _salary: '350000',
-            _startDate: '29 October 2019',
-            _note: '',
-            _id: new Date().getTime() + 1,
-            _profilePic: '../assets/profile-images/Ellipse -1.png'
-        }
-    ];
-    return employeePayrollListLocal;
 }
